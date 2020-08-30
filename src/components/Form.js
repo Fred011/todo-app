882c92ec86545fe6c86ea2f8a578b5fe0b70caf8
Fred011 <frederic.vannier01@gmail.com>
2020-08-30 11:40:07 +0200

    input text value fixed

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 const Form = ({ setInputText, inputText, setTodos, todos }) => {
   const inputTextHandler = (e) => {
     console.log(e.target.value);
-    setInputText(e);
+    setInputText(e.target.value);
   };
 
   const submitTodoHandler = (e) => {
@@ -12,15 +12,17 @@ const Form = ({ setInputText, inputText, setTodos, todos }) => {
       ...todos,
       { text: inputText, completed: false, id: Math.random() * 1000 },
     ]);
+    setInputText("");
   };
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
-      <button
-        onClick={submitTodoHandler}
-        className="todo-button"
-        type="submit"
-      >
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
